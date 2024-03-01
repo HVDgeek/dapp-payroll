17ce458126c0b7da88c8d989de28c28352c362a4
Hiduino Domingos <hvduino@gmail.com>
2024-03-01 00:28:59 -0300

    Create project structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import Header from "./components/Header"
+import Sidebar from "./components/Sidebar"
+import Dashboard from "./pages/Dashboard"
+import Footer from "./components/Footer"
+
+const App = () => {
+  return (
+    <div className="flex bg-[#f1f1f9] min-h-screen relative">
+      <Sidebar/>
+      <main className="flex flex-col space-y-3 w-full overflow-hidden">
+        <Header/>
+        <div className="p-5 sm:px-20">
+          <Routes>
+            <Route path="/" element={<Dashboard/>}/>
+          </Routes>
+        </div>
+        <div className="lg:hidden h-20"></div>
+        <Footer/>
+      </main>
+    </div>
+  )
+}
+
+export default App
