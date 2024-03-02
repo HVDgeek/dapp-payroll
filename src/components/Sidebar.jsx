019572e588ccf32ab9f64bb1d195b2fd84de0a1b
Hiduino Domingos <hvduino@gmail.com>
2024-03-02 01:49:49 -0300

    Implement Sidebar component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,65 @@
+import { NavLink } from "react-router-dom";
+import { FaEthereum } from "react-icons/fa";
+import { SlOrganization } from "react-icons/sl";
+import { FiBook } from "react-icons/fi";
+import { HiOutlineHome } from "react-icons/hi";
+import PropTypes from "prop-types";
+
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <aside className="lg:block hidden border-r border-gray-300">
+      <header
+        className="flex justify-between items-center space-x-2 border-b
+         border-gray-300 px-5 py-[1.6rem]"
+      >
+        <div className="flex justify-center items-center space-x-1">
+          <FaEthereum className="text-purple-700" size={20} />
+          <span>Dapp</span>
+          <span className="ml-1">Payroll</span>
+        </div>
+      </header>
+      <div>
+        <NavItem
+          Icon={<HiOutlineHome size={20} />}
+          route="/"
+          label={"Dashboard"}
+          isActive
+        />
+        <NavItem
+          Icon={<SlOrganization size={20} />}
+          route="/organizations"
+          label={"Organizations"}
+        />
+        <NavItem
+          Icon={<FiBook size={20} />}
+          route="/payrolls"
+          label={"Payrolls"}
+        />
+      </div>
+    </aside>
+  );
 }
+
+const NavItem = ({ Icon, route, label, isActive }) => {
+  const inactiveClass = `flex justify-start items-center space-x-1 rounded-full hover:bg-gray-200
+  p-3 font-semibold hover:text-purple-700 my-1 transition ease-in-out`;
+
+  const activeClass = `flex justify-start items-center space-x-1 rounded-full hover:bg-gray-200
+  p-3 font-semibold text-purple-700 my-1 transition ease-in-out`;
+
+  return (
+    <NavLink to={route} className={isActive ? activeClass : inactiveClass}>
+      {Icon}
+      <span>{label}</span>
+    </NavLink>
+  );
+};
+
+NavItem.propTypes = {
+  Icon: PropTypes.element.isRequired,
+  label: PropTypes.string.isRequired,
+  route: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+};
 
 export default Sidebar;
