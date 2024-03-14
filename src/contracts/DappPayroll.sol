8ac85fbe28a81962b3062ca78ece49a618420a86
Hiduino Domingos <hvduino@gmail.com>
2024-03-14 03:08:15 -0300

    Create `OrganizationStruct`  `PayrollStruct` and `WorkerStruct`

--- src/contracts/DappPayroll.sol
+++ src/contracts/DappPayroll.sol
@@ -1,6 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
+/*
+import "@openzeppelin/contracts/access/Ownable.sol";: This line imports the Ownable contract from OpenZeppelin,
+which provides functionalities for access control to the contract,
+allowing only the owner to execute certain actions.
+
+import "@openzeppelin/contracts/utils/Counters.sol";: This line imports the Counters contract from OpenZeppelin,
+which provides functionalities for secure counters.
+
+using Counters for Counters.Counter;: This line enables the use of the Counters.Counter
+data type from the Counters contract.
+ */
+
+
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
@@ -11,5 +24,49 @@ contract DappPayroll is Ownable, ReentrancyGuard {
   Counters.Counter private _totalPayrolls;
   Counters.Counter private _totalOrganizations;
   Counters.Counter private _totalWorkers;
+
+  enum Status {
+    OPEN,
+    PENDING,
+    DELECTED,
+    APPROVED,
+    REJECTED,
+    PAID
+  }
+
+  struct OrganizationStruct {
+    uint id;
+    string name;
+    string description;
+    address account;
+    uint payrolls;
+    uint workers;
+    uint payments;
+    uint balance;
+    uint cuts;
+    uint timestamp;
+  }
+
+  struct PayrollStruct {
+    uint id;
+    uint oid;
+    string name;
+    string description;
+    address officer;
+    address organization;
+    uint salary;
+    uint workers;
+    uint cut;
+    Status status;
+    uint timestamp;
+  }
+
+  struct WorkerStruct {
+    uint id;  // unique identify id
+    uint wid; // id in the payroll
+    string name;
+    address account;
+    uint timestamp;
+  }
 
 }
