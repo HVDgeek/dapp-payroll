8702e9e4b5712ad9293181dea86c0432681796ae
Hiduino Domingos <hvduino@gmail.com>
2024-03-19 06:18:31 -0300

    Smart contract deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const { ethers } = require("hardhat");
 const fs = require("fs");
 
 async function main() {
-  const contract_name = "";
-  const Contract = await ethers.getContractFactory(contract_name);
+  const contractName = "DappPayroll";
+  const Contract = await ethers.getContractFactory(contractName);
   const contract = await Contract.deploy();
 
   await contract.deployed();
 
   const address = JSON.stringify({ address: contract.address }, null, 4);
-  fs.writeFile("./src/abis/contractAddress.json", address, "utf8", (err) => {
+  fs.writeFile("./src/abis/contractAddress.json", address, "utf-8", (err) => {
     if (err) {
       console.error(err);
       return;
